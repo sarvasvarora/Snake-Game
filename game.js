36049d93a9651277fa062040db526b20ce673912
sarvasvarora <sarvasvarora@gmail.com>
2020-05-30 02:58:39 +0530

    added game mechanics

--- game.js
+++ game.js
@@ -1,36 +1,171 @@
 // get canvas and set context
-let canvas = document.getElementById("board");
-let context = canvas.getContext("2d");
+const canvas = document.getElementById("board");
+const context = canvas.getContext("2d");
 
 // get images
-let board = new Image();
+const board = new Image();
 board.src = "images/ground.png";
 
-let food = new Image();
-food.src = "images/food.png";
+const foodImage = new Image();
+foodImage.src = "images/food.png";
 
 // get audio files
-let up = new Audio("audio/up.mp3");
-let down = new Audio("audio/down.mp3");
-let left = new Audio("audio/left.mp3");
-let right = new Audio("audio/right.mp3");
-let eat = new Audio("audio/eat.mp3");
-let dead = new Audio("audio/dead.mp3");
+const up = new Audio("audio/up.mp3");
+const down = new Audio("audio/down.mp3");
+const left = new Audio("audio/left.mp3");
+const right = new Audio("audio/right.mp3");
+const eat = new Audio("audio/eat.mp3");
+const dead = new Audio("audio/dead.mp3");
 
 // one unit on game board = 32px 
-let box = 32;
+const box = 32;
 
 // score
 let score = 0
 
-// draw function
-function draw(){
-    context.drawImage(board, 0, 0);
-    context.
+//snake
+let snake = [{
+        x: 8 * box,
+        y: 10 * box
+    },
+    {
+        x: 7 * box,
+        y: 10 * box
+    }
+];
 
+
+// food
+let food = {};
+
+// function to generate new food randomly 
+function generateFood() {
+    food = {
+        x: Math.floor(Math.random() * 17 + 1) * box,
+        y: Math.floor(Math.random() * 15 + 3) * box
+    };
 }
 
-setInterval(draw, 100);
+// direction
+let direction = "right";
+
+// function to upddate snake's position
+function updateSnakePosition() {
+    let currentSnakeHeadX = snake[0].x;
+    let currentSnakeHeadY = snake[0].y;
+    switch (direction) {
+        case "left":
+            snake.unshift({
+                x: currentSnakeHeadX - box,
+                y: currentSnakeHeadY
+            });
+            break;
+        case "right":
+            snake.unshift({
+                x: currentSnakeHeadX + box,
+                y: currentSnakeHeadY
+            });
+            break;
+        case "up":
+            snake.unshift({
+                x: currentSnakeHeadX,
+                y: currentSnakeHeadY - box
+            });
+            break;
+        case "down":
+            snake.unshift({
+                x: currentSnakeHeadX,
+                y: currentSnakeHeadY + box
+            });
+            break;
+        default:
+            break;
+    }
+}
+
+// function to check collisions and food being eaten
+function check() {
+    // if snake head lands on food, level up the snake (i.e. change snake position without popping an element)
+    if (snake[0].x === food.x && snake[0].y === food.y) {
+        updateSnakePosition();
+        score++;
+        generateFood();
+        return;
+    }
+    if (snake[0].x < box || snake[0].x > 17 * box || snake[0].y < 3 * box || snake[0].y > 17 * box) {
+        clearInterval(game);
+        let finalScore = score;
+        context.font = "30px sans-serif";
+        score = `GAME OVER! Final Score: ${finalScore}`;
+        return;
+    }
+    for (let i = 1; i < snake.length; i++) {
+        if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+            clearInterval(game);
+            let finalScore = score;
+            context.font = "30px sans-serif";
+            score = `GAME OVER! Final Score: ${finalScore}`;
+            return;
+        }
+    }
+}
+
+// draw function
+function draw() {
+    // draw the board and food position
+    context.drawImage(board, 0, 0);
+    context.drawImage(foodImage, food.x, food.y);
+
+    // draw snake
+    for (let i = 0; i < snake.length; i++) {
+        context.fillStyle = i === 0 ? "green" : "white";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+
+        context.strokeStyle = "red";
+        context.strokeRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    // display current score
+    context.fillStyle = "white";
+    context.font = "40px sans-serif";
+
+    // update snake position and check for collisions or level up
+    snake.pop();
+    updateSnakePosition();
+    check();
+    context.fillText(score, 2.25 * box, 1.75 * box);
+}
+
+window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "ArrowLeft":
+            if (direction !== "right") {
+                direction = "left";
+            }
+            break;
+        case "ArrowUp":
+            if (direction !== "down") {
+                direction = "up";
+            }
+            break;
+        case "ArrowRight":
+            if (direction !== "left") {
+                direction = "right";
+            }
+            break;
+        case "ArrowDown":
+            if (direction !== "up") {
+                direction = "down";
+            }
+            break;
+        default:
+            break;
+    }
+});
+
+// generate first random apple and start game
+generateFood();
+let game = setInterval(draw, 100);
 
 /*
 canvas.getContext("2d")
@@ -48,4 +183,26 @@ context.font
 context.fillText
 
 
+*/
+
+/*
+LOCAL
+    git init
+    git config --global user.name 'github-username'
+    git config --global user.email 'github-email'
+    git add <filename> OR git add . [for all changes]
+    git status [to see current working tree i.e. things ready to be committed]
+    git commit -m 'commit message'
+
+    git branch <branch-name> [to create a new branch]
+    git checkout <branch-name> [to switch from master to another branch named <branch-name>]
+
+    git branch -b <new-branch-name> [shorthand for above two commands. use this for creating a new branch and going there simultaneously]
+
+
+REMOTE
+    git remote add origin -u https://...
+    git remote show origin
+    git push -u origin <branch-name> [branch-name would be master if pushing directly to master branch]
+    git pull origin --rebase [use this to pull files from remote repository if it is not in sync with local repository]
 */
